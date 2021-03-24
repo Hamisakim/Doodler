3c34e474a7384a4f5f74ffee38c46d478afca83c
Eric Petsopoulos <epetsopoulos@gmail.com>
2021-03-24 16:28:31 +0000

    backend added

---
+++ backend/controllers/artworkController.js
@@ -0,0 +1,18 @@
+import Artwork from '../models/artworkModel.js'
+
+export const getAllArtwork = async(req, res) => {
+  const artwork = await Artwork.find()
+  return res.status(200).json(artwork)
+}
+
+export const addArtwork = async(req, res) => {
+  try {
+    const newArtwork = { ...req.body }
+    const artworkToAdd = await Artwork.create(newArtwork)
+    return res.status(201).json(artworkToAdd)
+  } catch (err) {
+    console.log('🆘 Cannot add new show')
+    console.log(err)
+    return res.status(422).json(err)
+  }
+}
