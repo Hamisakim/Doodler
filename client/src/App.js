fff7cfa0c683ccc1cb7f4633d8b0715a60d59aba
hamisakim <samishakim@gmail.com>
2021-03-24 18:00:03 +0000

    pushing minimal canvas componet

--- client/src/App.js
+++ client/src/App.js
@@ -2,30 +2,61 @@ import React, { useState, useEffect, useRef } from 'react'
 import CanvasDraw from './drawing/index'
 // import { GithubPicker, TwitterPicker, CompactPicker, SliderPicker, SketchPicker } from 'react-color'
 import { ChromePicker } from 'react-color'
-
+import axios from 'axios'
 
 function App() {
-  const [color, setColor] = useState('#000')
-
-  let doodleRef = useRef(null)
-  let doodleShow = useRef(null)
+  const [color, setColor] = useState('#000') //* setting initial color for the brush 
+  // eslint-disable-next-line no-unused-vars
+  const [artwork, setArtwork] = useState('') //* setting state for artwork which we get from the built in  CanvasDraw package 
+  let doodleRef = useRef(null) //? top canvas = doodleRef DON'T MESS WITH IT 
+  let doodleShow = useRef(null)//? show canvas = doodleShow DON'T MESS WITH IT 
 
   useEffect(() => {
     setColor(color)
   }, [])
 
+
+  const handleSave = () => {
+    localStorage.setItem(
+      'savedDrawing',
+      doodleRef.getSaveData(),
+      console.log('save data', doodleRef.getSaveData())
+    )
+    // const artwork = localStorage.getItem('savedDrawing')
+    const artwork =  doodleRef.getSaveData()
+    console.log('🤖 ~ file: App.js ~ line 26 ~ artwork', artwork)
+    
+    const sendArtwork = async(artwork) => {
+      const response = await axios.post('api/artwork', artwork)
+      console.log('🤖 ~ file: App.js ~ line 30 ~ response', response)
+
+    }
+    sendArtwork()
+  }
+
+
+
   return (
     <>
       <div>
-        <button
-          onClick={() => {
-            localStorage.setItem(
-              'savedDrawing',
-              doodleRef.getSaveData(),
-              console.log('save data', doodleRef.getSaveData())
-            )
-          }}
+        <button onClick={() => {
+          handleSave()
+        }}
+
+
+
+          // onClick={() => {
+          //   localStorage.setItem(
+          //     'savedDrawing',
+          //     doodleRef.getSaveData(),
+          //     console.log('save data', doodleRef.getSaveData())
+          //   )
+          // }}
         > Save </button>
+       
+       
+       
+       
         <button
           onClick={() => {
             doodleRef.clear()
