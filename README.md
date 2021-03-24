d52b78af19d08fbe442a30cf9cfc19c501bd266d
hamisakim <samishakim@gmail.com>
2021-03-24 14:44:49 +0000

    pushing

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# doodle
-doodle sei project 3 
-My group project
-Eric  --- Hello eric 
-sami
+# ![](https://ga-dash.s3.amazonaws.com/production/assets/logo-9f88ae6c9c3871690e33280fcf557f33.png) GA London React Template
+
+`yarn start` to run the development server
+
+`yarn build` to create a build directory
+
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect, useRef } from 'react'
+import CanvasDraw from './drawing/index'
+// import { GithubPicker, TwitterPicker, CompactPicker, SliderPicker, SketchPicker } from 'react-color'
+// eslint-disable-next-line no-unused-vars
+import { CompactPicker, MaterialPicker, ChromePicker } from 'react-color'
+
+
+function App() {
+  const [color, setColor] = useState('#000')
+
+  let doodleRef = useRef(null)
+  let doodleShow = useRef(null)
+
+  useEffect(() => {
+    setColor(color)
+  }, [])
+
+  return (
+    <>
+      <div>
+        <button
+          onClick={() => {
+            localStorage.setItem(
+              'savedDrawing',
+              doodleRef.getSaveData(),
+              console.log('save data', doodleRef.getSaveData())
+            )
+          }}
+        >
+            Save
+        </button>
+        <button
+          onClick={() => {
+            doodleRef.clear()
+          }}
+        >
+            Clear
+        </button>
+        <button
+          onClick={() => {
+            doodleRef.undo()
+          }}
+        >
+            Undo
+        </button>
+      </div>
+      {/* <GithubPicker />
+      <TwitterPicker /> */}
+      {/* <CompactPicker 
+        color={color}
+        onChangeComplete={color => {
+          setColor(color.hex)
+        }}
+      /> */}
+      <ChromePicker
+        color={color}
+        onChangeComplete={color => {
+          setColor(color.hex)
+        }}
+      />
+
+      {/* <SliderPicker /> */}
+      {/* <SketchPicker /> */}
+      <CanvasDraw
+        ref={canvasDraw => (doodleRef = canvasDraw)}
+        brushColor={color}
+      />
+      <button
+        onClick={() => {
+          doodleShow.loadSaveData(
+            localStorage.getItem('savedDrawing')
+          )
+        }}
+      >
+            Display saved doodle
+      </button>
+      <CanvasDraw
+        ref={canvasDraw => (doodleShow = canvasDraw)}
+        disabled
+        hideGrid 
+        saveData={localStorage.getItem('savedDrawing')}
+      />
+      
+    </>
+  )
+}
+
+export default App
