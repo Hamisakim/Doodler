a153dde61c46c024aea499fdad274148e0b788e5
Eric Petsopoulos <epetsopoulos@gmail.com>
2021-03-25 16:20:31 +0000

    bug fixed

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,6 +1,3 @@
-//import React, { Component } from 'react'
-//import { render } from 'react-dom'
-
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
@@ -22,7 +19,6 @@ const Gallery = () => {
     <div className='columns'>
       <div className='column'>
         {doodles.map(artwork => {
-          //if (artwork === undefined) return null
           return  <div div key={artwork._id}>
             <p>{artwork.title}</p>
             <CanvasDraw       
@@ -38,50 +34,4 @@ const Gallery = () => {
   )
 }
 
-// class Gallery extends Component {
-//   state = {
-//     allArtwork: [],
-//     formData: {
-//       title: '',
-//       description: '',
-//       doodleData: ''
-//     }
-//   }
-//   componentDidMount() {
-//     const getDoodles = async () => {
-//       try {
-//         const res = await fetch('api/artwork')
-//         const data = await res.json()
-//         this.setState({ allArtwork: data })
-//         //console.log(data)
-//         //console.log(data[4].doodleData)
-//       } catch (err) {
-//         console.log(err)
-//       }
-//     }
-//     getDoodles()
-//   }
-//   render() {
-//     console.log('this.state.allArtwork ->', this.state.allArtwork)
-//     //if (this.state.allArtwork[0] === undefined) return <div>hi</div>
-//     return (
-//       <div className='columns'>
-//         <div className='column'>
-//           {this.state.allArtwork.map(artwork => {
-//             //if (artwork === undefined) return null
-//             return  <div div key={artwork._id}>
-//               <p>{artwork.title}</p>
-//               <CanvasDraw       
-//                 disabled
-//                 hideGrid
-//                 saveData={artwork.doodleData}
-//                 //saveData={artwork.doodleData}
-//               />
-//             </div>
-//           })}
-//         </div>
-//       </div>
-//     )
-//   }
-// }
 export default Gallery
