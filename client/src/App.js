308e7e99361dbca4a877b516f8571ba2b03c0b67
hamisakim <samishakim@gmail.com>
2021-03-25 09:39:17 +0000

    hello

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,25 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Canvas from './Canvas'
 import Gallery from './components/Gallery'
-
+import HomePage from './components/HomePage'
 const App = () => {
  
   return (
     <>
       <BrowserRouter>
         <Switch>
+          <Route path='/'>
+            <HomePage />
+          </Route>
+
           <Route path='/gallery'>
             <Gallery/>
-      
           </Route>
 
           <Route path='/canvas'>
             <Canvas/>
-      
           </Route>
+
         </Switch>
       </BrowserRouter>
 
