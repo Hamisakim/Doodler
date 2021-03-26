89f2d06a58af14de9dd3c9a6d81640962ca902cf
hamisakim <samishakim@gmail.com>
2021-03-26 10:24:18 +0000

    add before merge

--- backend/models/artworkModel.js
+++ backend/models/artworkModel.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose'
 
 const artworkSchema = new mongoose.Schema({
-  title: { type: String, required: false, maxlength: 60 },
+  title: { type: String, required: true, maxlength: 60 },
   description: { type: String, required: false, maxlength: 500 },
-  doodleData: { type: String, required: true }
+  doodleData: { type: Object, required: true }
 },
 { timestamps: true }
 )
