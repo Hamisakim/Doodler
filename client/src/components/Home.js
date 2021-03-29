0beefc49c897f0fa02e56663368e5821c09e09a7
Ayo Olawoye <ayo.olawoye@gmail.com>
2021-03-29 05:59:31 +0100

    links

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,42 +1,54 @@
-import '../styles/componentStyles/home.scss'
 import React from 'react'
+import '../styles/componentStyles/home.scss'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
 
 
   return (
     <>
+      <div className="section-header">
+        <h2 className="section-title">
+          <img src="https://i.imgur.com/wNw1Uug.jpg" alt="Airbnb Logo" className="second-logo"></img>
+        </h2>
+      </div>
       <section className="hero is-medium is-link">
         <div className="hero-body" id="hero-body">
-          {/* <p className="title">
-            Medium hero
-          </p> */}
-          {/* <p className="subtitle">
-            Medium subtitle
-          </p> */}
         </div>
 
       </section>
       <section className="section">
-        <div className="section-header">
-          <h2 className="section-title">
-            <img src="https://i.imgur.com/wNw1Uug.jpg" alt="Airbnb Logo" className="second-logo"></img>
-          </h2>
-        </div>
         <ul className="grid">
           <li className="grid-item">
-            <div className="homepage page-doodle">
+            <Link to="/doodle" className="homepage page-doodle">
               <h3 className="homepage-title shadow">Doodle</h3>
-            </div>
+            </Link>
           </li>
           <li className="grid-item">
-            <div className="homepage page-gallery">
+            <Link to="/gallery" className="homepage page-gallery">
               <h3 className="homepage-title shadow">Gallery</h3>
-            </div>
+            </Link>
           </li>
-
         </ul>
+      </section>
+      <section>
+        // To use lazy loading, set a data-lazy attribute
+        // on your img tags and leave off the src
 
+        <img data-lazy="img/lazyfonz1.png" />
+
+        {/* $('.lazy').slick({
+          lazyLoad: 'ondemand',
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }); */}
+
+        $('.autoplay').slick({
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2000,
+        });
       </section>
     </>
   )
