25edbd92df392f1af4554d8f3501a7bdeba611ec
Aida Bourdis <aida.lucie@gmail.com>
2021-03-29 10:19:16 +0100

    profile with form, not currently working

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,8 +1,10 @@
+import '../styles/componentStyles/profile.scss'
+
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import ArtCard from './ArtCard'
 
-const Profile = ({ username } ) => {
+const Profile = () => {   //{ username } 
 
   const [artwork, setArtwork] = useState(null)
 
@@ -14,49 +16,59 @@ const Profile = ({ username } ) => {
     getData()
   }, [])
 
+  // const [formData, setFormData] = useState({
+  //   description: '',
+  // })
+  // const handleChange = (event) => {
+  //   const newFormData = { ...formData, [event.target.name]: event.target.value }
+  //   setFormData(newFormData)
+  // }
+  // const handleSave = () => {
+  //   const artworkToSend = doodle.getSaveData()
+  //   const newFormData = { ...formData, doodleData: artworkToSend, formData }
+  //   setFormData(newFormData)
+
   return (
-    <div className="section">
-      <div className="box">
-        <div className="tile is-vertical">
-          <div className="tile">
-            <figure className="image is-128x128">
-              <img src="https://bulma.io/images/placeholders/128x128.png"></img>
-            </figure>
-          </div>
-          <div>
-            <h1>User Info</h1>
-            <h1>{username}</h1>
-          </div>
-          <div>
-            <h1>This is my profile, and this is my bio. Lalalalalalalala. Spring is in the air, and I am a flower, with nothing interesting to say.</h1>
-          </div>
-        </div>
-      </div>
-      <div className="box">
-        <h1>Your Doodles</h1>
-        <div>
-          { artwork &&
-            <div className="columns">
-              { artwork.map( art => (
-                <ArtCard key={art._id} {...art} />
-              ))}
+
+    <div className="main">
+      <div className="section">
+        <div className="box">
+          <div className="tile is-vertical">
+            <div className="tile">
+              <h1>username</h1>
+              <figure className="image is-128x128">
+                <img src="https://bulma.io/images/placeholders/128x128.png"></img>
+              </figure>
             </div>
-          }
-        </div>
-      </div>
-      <div className="box">
-        <h1>Your Favourites</h1>
-        <div>
-          { artwork &&
-            <div className="columns">
-              { artwork.map( art => (
-                <ArtCard key={art._id} {...art} />
-              ))}
+            <div>
+              <h1 className="title">User Info</h1>
             </div>
-          }
+            {/* <div>
+              <input
+                className="input"
+                placeholder="Description"
+                name="description"
+                value={formData.description}
+                onChange={handleChange}
+              />
+            </div> */}
+          </div>
         </div>
+        <div className="box">
+          <h1>Your Doodles</h1>
+          <div>
+            { artwork &&
+              <div className="columns">
+                { artwork.map( art => (
+                  <ArtCard key={art._id} {...art} />
+                ))}
+              </div>
+            }
+          </div>
+        </div>
+       
       </div>
-    </div>
+    </div>  
   )
 }
 
