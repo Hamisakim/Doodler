31e3c568984ae91e75d629831b93ba415d57795f
Ayo Olawoye <ayo.olawoye@gmail.com>
2021-03-30 10:57:11 +0100

    tuesday morning updates

--- client/src/components/userStuff/Carousel.js
+++ client/src/components/userStuff/Carousel.js
@@ -10,15 +10,15 @@ const DemoCarousel = () => {
 
       <div>
         <img className="carousel-images" src="https://i.imgur.com/Huco3rp.jpg" />
-        <p className="legend">Legend 1</p>
+
       </div>
       <div>
         <img className="carousel-images" src="https://i.imgur.com/g77ip9x.jpg" />
-        <p className="legend">Legend 2</p>
+
       </div>
       <div>
         <img className="carousel-images" src="https://i.imgur.com/H7iBOQY.jpg" />
-        <p className="legend">Legend 3</p>
+
       </div>
 
     </Carousel>
