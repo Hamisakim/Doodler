5c59263f68e0c21bebb22b7fe170c32b5706531f
hamisakim <samishakim@gmail.com>
2021-03-31 17:21:25 +0100

    efefe

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ const startServer = async() => {
     app.use('/api', router)
 
     // * Server
-    app.listen(port, () => console.log(`🚀 Express is up and running on port ${port}`🚀))
+    app.listen(port, () => console.log(`🚀 Express is up and running on port ${port}🚀`))
   } catch (err) {
     console.log('🆘 Something went wrong starting the app')
     console.log(err)
