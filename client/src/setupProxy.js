3eafa697dc3d38759d089fab5c406def8bb84bcf
hamisakim <samishakim@gmail.com>
2021-04-27 20:06:42 +0100

    m

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
-  app.use(createProxyMiddleware('/api', { target: 'https://sei-doodle-server.herokuapp.com/api' }))
+  app.use(createProxyMiddleware('/api', { target: 'https://sei-doodle-server.herokuapp.com/' }))
 }
 
 //* _______Original_____
