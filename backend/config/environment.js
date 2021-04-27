11ce53bb64fc4f2bc9a8ef8df983da9a4e3d7e90
hamisakim <samishakim@gmail.com>
2021-04-27 17:04:34 +0100

    puhs

--- backend/config/environment.js
+++ backend/config/environment.js
@@ -1,3 +1,5 @@
-export const port = 4000
-export const dbURI = 'mongodb://localhost/doodle-app' // * address of where our db lives, creates db with the name we give here
+// export const port = 4000
+// export const dbURI = 'mongodb://localhost/doodle-app' // * address of where our db lives, creates db with the name we give here
 export const secret = 'This is a secret'
+export const port = process.env.PORT || 4000
+export const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/doodle-app'
